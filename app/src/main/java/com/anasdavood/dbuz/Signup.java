93e03b1bde6a72f8f4af9201a6ed26e58d49ddee
ANAS DAVOOD TK <anasdavoodtk1@gmail.com>
2020-12-14 15:45:19 +0530

    commit,01-14/12/2020

---
+++ app/src/main/java/com/anasdavood/dbuz/Signup.java
@@ -0,0 +1,16 @@
+package com.anasdavood.dbuz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    }
+}
