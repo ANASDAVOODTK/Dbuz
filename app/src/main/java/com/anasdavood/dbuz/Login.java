5776e871d14eae82f91aed26e9735305786ad7fa
ANAS DAVOOD TK <anasdavoodtk1@gmail.com>
2020-12-15 23:04:18 +0530

    commit,04-14/12/2020

--- app/src/main/java/com/anasdavood/dbuz/Login.java
+++ app/src/main/java/com/anasdavood/dbuz/Login.java
@@ -1,20 +1,46 @@
 package com.anasdavood.dbuz;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+
+    EditText email,password;
+    ProgressBar progressBar;
+    FirebaseAuth mfirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+        email=findViewById(R.id.edtmail);
+        password=findViewById(R.id.edtpassword);
+
         TextView txtlogin =  findViewById(R.id.txtsingup);
         txtlogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,12 +50,90 @@ public class Login extends AppCompatActivity {
             }
         });
 
+
+        mfirebaseAuth = FirebaseAuth.getInstance();
+
+        progressBar = (ProgressBar)findViewById(R.id.loading);
+
+        progressBar.setVisibility(View.GONE);
+
+        TextView forhet = findViewById(R.id.txtforgget);
+        forhet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final EditText sendmail = new EditText(view.getContext());
+                final AlertDialog.Builder resetdailog = new AlertDialog.Builder(view.getContext());
+                resetdailog.setTitle("Reset Password");
+                resetdailog.setMessage("Enter your E-mail to Received Link");
+                resetdailog.setView(sendmail);
+                resetdailog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String email = sendmail.getText().toString();
+                        mfirebaseAuth.sendPasswordResetEmail(email).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(Login.this,"Reset link send your email",Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Login.this,"Error! Reset link not send"+e.getMessage(),Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+                    }
+                });
+                resetdailog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                resetdailog.create().show();
+
+            }
+        });
+
+
+
         View vvlogin =  findViewById(R.id.vvlogin);
         vvlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Login.this, Home.class);
-                startActivity(i);
+                String memail = email.getText().toString().trim();
+                String mpassword = password.getText().toString().trim();
+
+                if (TextUtils.isEmpty(memail)){
+                    email.setError("E-mail is Required");
+
+                }
+                else if (TextUtils.isEmpty(mpassword)){
+                    password.setError("Password is Required");
+                }
+
+                else  if (password.length() < 8){
+                    password.setError("Paasword is Short to 8");
+                }
+                else {
+                    progressBar.setVisibility(View.VISIBLE);
+
+                    mfirebaseAuth.signInWithEmailAndPassword(memail, mpassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(Login.this, "Logged is Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), Home.class));
+                                finish();
+                            } else {
+                                Toast.makeText(Login.this, "Eorror !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.GONE);
+                            }
+                        }
+
+                    });
+                }
             }
         });
     }
--- app/src/main/java/com/anasdavood/dbuz/Signup.java
+++ app/src/main/java/com/anasdavood/dbuz/Signup.java
@@ -1,37 +1,242 @@
 package com.anasdavood.dbuz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class Signup extends AppCompatActivity {
+
+    EditText name,email,password;
+
+    FirebaseAuth firebaseAuth;
+    private String TAG = "TAG";
+    ProgressBar progressBar;
+    SharedPreferences sp1;
+    private GoogleSignInClient mGoogleSignInClient;
+    private final static int RC_SIGN_IN = 123;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        sp1 = getSharedPreferences("PDATA", Context.MODE_PRIVATE);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        createRequest();
+
+
+        name=findViewById(R.id.username);
+        email=findViewById(R.id.edtmail);
+        password=findViewById(R.id.edtpassword);
 
         TextView txtlogin =  findViewById(R.id.txtlogin);
         txtlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Signup.this, Login.class);
-                startActivity(i);
+
+                Intent intent = new Intent(Signup.this, Login.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+
             }
         });
+
+
+        progressBar = (ProgressBar)findViewById(R.id.loading);
+
+        progressBar.setVisibility(View.GONE);
+
+
+
+
+
+
+        if (firebaseAuth.getCurrentUser() !=null){
+            startActivity(new Intent(getApplicationContext(),Home.class));
+            finish();
+        }
+
 
         View vvsingup =  findViewById(R.id.vvsingup);
         vvsingup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Signup.this, Home.class);
-                startActivity(i);
+                String memail = email.getText().toString().trim();
+                String mpassword = password.getText().toString().trim();
+
+
+
+                if (TextUtils.isEmpty(memail)) {
+                    name.setError("name is Required");
+                }
+
+                else if (TextUtils.isEmpty(memail)) {
+                    email.setError("E-mail is Required");
+                }
+                else if (TextUtils.isEmpty(mpassword)){
+                    password.setError("Password is Required");
+                }
+
+                else  if (password.length() < 8){
+                    password.setError("Paasword is Short to 8");
+                }
+                else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    firebaseAuth.createUserWithEmailAndPassword(memail,mpassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+                                firebaseUser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Toast.makeText(Signup.this,"E-mail Verification Link Send",Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.d(TAG,"E-mail not send"+e.getMessage());
+                                    }
+                                });
+                                String name1 = name.getText().toString().trim();
+                                SharedPreferences.Editor editor= sp1.edit();
+                                editor.putString("name",name1);
+                                editor.commit();
+
+                                Toast.makeText(Signup.this,"E-mail Verification Link Send",Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(),Home.class));
+                                finish();
+                            }else {
+                                Toast.makeText(Signup.this,"Eorror !"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.GONE);
+                            }
+                        }
+                    });}
             }
         });
+
+
+        ConstraintLayout gsign = findViewById(R.id.Googlesign);
+
+        gsign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                signIn();
+            }
+        });
+
+
+
+    }
+
+    private void createRequest() {
+
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+    }
+
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = (GoogleSignInAccount) task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (Throwable e) {
+                // Google Sign In failed, update UI appropriately
+                // ...
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(this, "Try Again", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(getApplicationContext(),Home.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+
+
+                        } else {
+                            progressBar.setVisibility(View.GONE);
+                            Toast.makeText(Signup.this, "Sorry auth failed.", Toast.LENGTH_SHORT).show();
+
+
+                        }
+
+
+                        // ...
+                    }
+                });
     }
 }
